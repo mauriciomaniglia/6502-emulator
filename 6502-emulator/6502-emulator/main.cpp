4b76b2611e28c3e5e45a577ce1db5caf4b9bbbd7
mauriciomaniglia <manigliamauricio@gmail.com>
2025-04-27 10:18:58 -0300

    Creating CPU internals

--- 6502-emulator/6502-emulator/main.cpp
+++ 6502-emulator/6502-emulator/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
+using Byte = unsigned char;
+using Word = unsigned short;
+
+struct CPU {
+    Word PC;        // program counter
+    Word SP;        // stack pointer
+
+    Byte A;         // registers
+    Byte X;
+    Byte Y;
+
+    Byte C : 1;     // status flags
+    Byte Z : 1;
+    Byte I : 1;
+    Byte D : 1;
+    Byte B : 1;
+    Byte V : 1;
+    Byte N : 1;
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    CPU cpu;
     return 0;
 }
