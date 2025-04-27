84b9f2462c807976320181f4beab24197b322a97
mauriciomaniglia <manigliamauricio@gmail.com>
2025-04-27 10:28:19 -0300

    Resetting the CPU

--- 6502-emulator/6502-emulator/main.cpp
+++ 6502-emulator/6502-emulator/main.cpp
@@ -18,9 +18,28 @@ struct CPU {
     Byte B : 1;
     Byte V : 1;
     Byte N : 1;
+
+    void Reset() {
+        PC = 0xFFFC;
+        SP = 0x0100;
+
+        D = 0;
+        A = 0;
+        X = 0;
+        Y = 0;
+
+        C = 0;
+        Z = 0;
+        I = 0;
+        D = 0;
+        B = 0;
+        V = 0;
+        N = 0;
+    }
 };
 
 int main(int argc, const char * argv[]) {
     CPU cpu;
+    cpu.Reset()
     return 0;
 }
