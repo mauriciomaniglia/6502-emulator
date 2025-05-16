34eb78a3985d7368ba77f35632f5a28638a1ab34
mauriciomaniglia <manigliamauricio@gmail.com>
2025-05-16 10:42:48 -0300

    Test LDA Immediate instruction can load a value into the A register

--- 6502-emulator/6502-emulator-test/6502-test.mm
+++ 6502-emulator/6502-emulator-test/6502-test.mm
@@ -19,18 +19,16 @@ CPU cpu;
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
-- (void)test_m6502_runALittleInlineProgram {
-    memory[0xFFFC] = CPU::INS_JSR;
-    memory[0xFFFD] = 0x42;
-    memory[0xFFFE] = 0x42;
-    memory[0x4242] = CPU::INS_LDA_IM;
-    memory[0x4243] = 0x84;
+- (void)test_LDAImmediate_canLoadAValueIntoTheARegister {
+    // Given
+    memory[0xFFFC] = CPU::INS_LDA_IM;
+    memory[0xFFFD] = 0x84;
 
-    // Execute 9 cycles
-    cpu.execute(9, memory);
+    // When
+    cpu.execute(2, memory);
 
-    // Assertions
-    XCTAssertEqual(cpu.A, 0x84, @"A register should contain 0x84 after LDA_IM");
+    // Then
+    XCTAssertEqual(cpu.A, 0x84);
 }
 
 @end
